6201ce82e9a59fb1b329f2eb3cf9e78f5a63d22b
Nguyễn Giang <101436766+MyTaeyeon@users.noreply.github.com>
2023-10-23 13:27:26 +0700

    PY01014 - CHIA HET CHO K

--- PY01014.py
+++ PY01014.py
@@ -1,6 +1,3 @@
 [a, b, c] = list(map(int, input().split()))
 ans = [str(x - a) for x in range((a + b) // b * b, c + 1, b)]
-if len(ans) == 0:
-    print(-1)
-else:
-    print(' '.join(ans))
+print(-1) if len(ans) == 0 else print(' '.join(ans))
