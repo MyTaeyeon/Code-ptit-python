edbeac129e29187436033c751a239b544dcac26b
Nguyễn An Bình <145442426+NguyennAnBinh@users.noreply.github.com>
2023-11-23 19:56:29 +0700

    PY02034 - DEM CAC SO CO HAI CHU SO

---
+++ PY02034.py
@@ -0,0 +1,11 @@
+s = input()
+a = []
+for i in range(len(s)//2):
+    b = int(s[i*2]) * 10 + int(s[i*2+1])
+    a.append(b)
+tmp = [0] * 100
+b = []
+for i in range(len(a)):
+    if ( tmp[a[i]] == 0 ):
+        tmp[a[i]] = 1
+        print(a[i], a.count(a[i]))
