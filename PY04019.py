aa27fbedc046934dd7279faf25c8c16d31209768
Nguyễn Giang <101436766+MyTaeyeon@users.noreply.github.com>
2023-12-15 09:56:56 +0700

    PY04019 - TUYEN NHAN VIEN

--- PY04019.py
+++ PY04019.py
@@ -1,10 +1,14 @@
+idx = 1
 class staff:
     scoreFormat = lambda x: x if x <= 10 else x / 10
 
-    def __init__(self, id, name, sc1, sc2) -> None:
-        self.id = f'TS{id:02d}'
+    def __init__(self, name, *scores) -> None:
+        global idx
+        self.id = f'TS0{idx:d}'
+        idx += 1
         self.name = name
-        self.score = (staff.scoreFormat(sc1) + staff.scoreFormat(sc2)) / 2
+        self.score = (staff.scoreFormat(scores[0]) + staff.scoreFormat(scores[1])) / 2
+        self.band = self.type(self.score)
 
     def type(self, score):
         if score < 5:
@@ -14,18 +18,19 @@ class staff:
         elif score < 9.5:
             return 'DAT'
         else:
-            return 'XUAT XAC'
+            return 'XUAT SAC'
         
     def __str__(self) -> str:
-        return f'{self.id} {self.name} {self.score:.2f} {self.type(self.score)}'
+        return f'{self.id} {self.name} {self.score:.2f} {self.band}'
 
 candidates = []
-for i in range(1, int(input()) + 1):
+tests = int(input())
+for i in range(tests):
     name = input()
     sc1 = float(input())
     sc2 = float(input())
-    candidates.append(staff(i, name, sc1, sc2))
+    candidates.append(staff(name, sc1, sc2))
 
-candidates = sorted(candidates, key=lambda x: x.score, reverse=True)
+candidates.sort(key=lambda x: x.score, reverse=True)
 
 print(*candidates, sep='\n')
